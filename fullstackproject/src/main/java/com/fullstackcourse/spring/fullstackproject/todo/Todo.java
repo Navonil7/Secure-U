8488d5b9fb77d840fe346f5157f1a1382d2d860f
Navonil Ghosh <navonilghosh6@gmail.com>
2020-06-06 18:31:29 +0530

    Implimenting Spring JPA

--- fullstackproject/src/main/java/com/fullstackcourse/spring/fullstackproject/todo/Todo.java
+++ fullstackproject/src/main/java/com/fullstackcourse/spring/fullstackproject/todo/Todo.java
@@ -2,16 +2,22 @@ package com.fullstackcourse.spring.fullstackproject.todo;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+@Entity
 public class Todo {
-	private long id;
+	@Id
+	@GeneratedValue
+	private Long id;
 	private String username;
 	private String description;
 	private Date targetDate;
 	
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 	public String getUsername() {
@@ -41,7 +47,7 @@ public class Todo {
 	private boolean isDone;
 
 	public Todo() {}
-	public Todo(long id, String username, String description, Date targetDate, boolean isDone) {
+	public Todo(Long id, String username, String description, Date targetDate, boolean isDone) {
 		super();
 		this.id = id;
 		this.username = username;
---
+++ fullstackproject/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo(id, username, description, target_date, is_done)
+values(1001, 'navonil', 'Retail', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(1002, 'navonil', 'Insurance', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(1003, 'navonil', 'BFS', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(1001, 'navonil', 'Testing', sysdate(), false);
