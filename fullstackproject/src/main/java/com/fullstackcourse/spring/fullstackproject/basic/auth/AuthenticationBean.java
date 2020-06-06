6fcaad1defe70287aa4c66f8bd516867ad5ebff3
Navonil Ghosh <navonilghosh6@gmail.com>
2020-06-06 13:28:58 +0530

    Commiting all SpringBoot implementation

---
+++ fullstackproject/src/main/java/com/fullstackcourse/spring/fullstackproject/basic/auth/AuthenticationBean.java
@@ -0,0 +1,23 @@
+package com.fullstackcourse.spring.fullstackproject.basic.auth;
+
+public class AuthenticationBean {
+	private String message;
+
+	public AuthenticationBean(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
---
+++ fullstackproject/src/test/java/com/fullstackcourse/spring/fullstackproject/FullstackprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackcourse.spring.fullstackproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
